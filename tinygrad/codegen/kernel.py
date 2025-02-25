8c50213848bbc9778890531670d2c89ab89dd810
guanhua <0guanhua0@gmail.com>
2025-02-25 23:50:23 +0000

    wip rev

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -401,7 +401,7 @@ class Kernel:
       check(axis < self.first_reduce, "upcast is for non-reduce")
       check(not (self.tensor_core and self.global_dims <= axis < self.global_dims+len(self.tensor_core.get_local_axes())), "can't upcast TC locals")
       check((self.opts is not None and self.opts.device == "DSP") or amt <= 16, "don't upcast more than 16")
-      # check
+      # todo(guanhua): check
       self.shift_to(axis, amt, insert_before=None)
       self.upcast()
     elif opt.op is OptOps.NOLOCALS:
